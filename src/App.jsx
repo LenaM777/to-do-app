accbac45d7a5c774d0869bf4c79301d731182ff4
Olena Mustafina <toysamiy.mail@gmail.com>
2025-05-06 14:34:56 +0300

    feat: Implement adding new todos

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,22 @@ import TodoForm from "./components/TodoForm";
 const App = () => {
   const [todos, setTodos] = useState([]);
 
+  function addTask(userInput) {
+    if (userInput.trim() !== "") {
+      const newTask = {
+        id: Date.now(),
+        task: userInput,
+        completed: false,
+      };
+
+      setTodos([newTask, ...todos]);
+    }
+  }
+
   return (
     <div>
       <h1>To-Do List</h1>
-      <TodoForm />
+      <TodoForm addTask={addTask} />
     </div>
   );
 };
